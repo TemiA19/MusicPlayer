497054bf8578a4a5dd452305f4b9191888d07b98
Temi Aminu <aminut254@mail.sacredheart.edu>
2022-11-10 20:40:42 -0500

    added new files

---
+++ data.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+        name: `Blue Lights`,
+        path: 'musics/music1.mp3',
+        artist: 'LizAnn',
+        cover: 'img/cover1.jpeg'
+    },
+    {
+        name: 'This Island',
+        path: 'musics/music2.mp3',
+        artist: 'Laurence DaNova',
+        cover: 'img/cover2.jpeg'
+    },
+    {
+        name: 'Fashion City',
+        path: 'musics/music3.mp3',
+        artist: 'BOULVARD X-AUDI',
+        cover: 'img/cover3.jpeg'
+    },
+    {
+        name: 'Energy',
+        path: 'musics/music4.mp3',
+        artist: 'Pokki DJ',
+        cover: 'img/cover4.jpeg'
+    },
+]
